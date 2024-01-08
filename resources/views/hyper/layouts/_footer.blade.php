c528a17f8ef02bc4af2b9cafe5b233b8bd9f07a1
Yu Lin <lywedo@gmail.com>
2024-01-08 21:59:15 +1300

    add configeration

--- resources/views/hyper/layouts/_footer.blade.php
+++ resources/views/hyper/layouts/_footer.blade.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-md-6">
             	<div class="footer-links">
-                    Powered by <a href="https://github.com/assimon/dujiaoka">@独角数卡.</a>
+                    Powered by Andrew
                 </div>
             </div>
             <div class="col-md-6">
